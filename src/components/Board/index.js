05db065865e321cb57e741f37cce420426c1a470
Vijay Kapoor <vijayk_1@hotmail.com>
2019-11-09 15:00:04 -0300

    refactor: Implementa corretamente componente

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -2,37 +2,25 @@ import React from "react";
 import Square from "../Square";
  
 class Board extends React.Component {
-    renderSquare = (i) => {
-        const {squares, onClick} = this.props;
-        return (
-            <Square 
-                value={squares[i]} 
+  render() {
+    const {squares, onClick} = this.props;
+    return (
+      <div>
+        {squares.map((square, index) => (
+          <div key={index} className="board-row">
+            {square.map((s, i) => (
+              <Square
+                key={i}
+                value={squares[index][i]}
+                position={{i: index, j: i}}
                 onClick={onClick}
-            />
-        );
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Board;

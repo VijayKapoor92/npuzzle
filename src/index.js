e6f1a96405cb7a930c822b1830b8da177149121b
Vijay Kapoor <vijayk_1@hotmail.com>
2019-11-16 19:20:48 -0300

    refactor: Implementa o importe correto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Game from './Game';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();

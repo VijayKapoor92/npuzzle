1b02b5e966dbdb84a145c67113a2a3f6205e11c2
Vijay Kapoor <vijayk_1@hotmail.com>
2019-11-24 21:20:16 -0300

    feat: Cria novo componente Modal

---
+++ src/components/Modal/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import CloseIcon from "react-icons/lib/fa/close";
+import TrophyIcon from "react-icons/lib/fa/trophy";
+import "./index.css";
+
+const Modal = ({open, title, children, winner, onClose}) => {
+  return (
+    <div className={`modal ${open ? "show" : ""}`}>
+      <div className={`modal-content ${!winner ? "modal-config" : ""}`}>
+        <div className="modal-dismiss" onClick={() => onClose()}>
+          <CloseIcon/>
+        </div>
+        <div className="modal-title">{title}</div>
+        {winner && (
+          <div>
+            <TrophyIcon style={{fontSize: 64}}/>
+          </div>
+        )}
+        <div className="modal-body">
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Modal;

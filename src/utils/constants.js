8e8714a20a138bdcad139c08b09c3e70149dd40c
Vijay Kapoor <vijayk_1@hotmail.com>
2019-11-24 21:19:05 -0300

    feat: Cria novas constantes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 export const
-  PUZZLE_MODE_EASY = "easy",
-  PUZZLE_MODE_MEDIUM = "medium",
-  PUZZLE_MODE_HARD = "hard"
+  PUZZLE_MODE_3X3 = "3x3",
+  PUZZLE_MODE_4X4 = "4x4",
+  PUZZLE_MODE_5X5 = "5x5",
+
+  STORAGE_DATABASE = "game",
+  STORAGE_CONFIG_DATABASE = "config"
 ;

146a6ea97c03e22f1426cbfec5157c54d13e2044
Vijay Kapoor <vijayk_1@hotmail.com>
2019-11-25 02:03:19 -0300

    Updates

---
+++ precache-manifest.f2f2393697eb89bebe921fadb7c651f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8b574a7c4b709b8ed4ecb0adf4df8a",
+    "url": "/npuzzles/index.html"
+  },
+  {
+    "revision": "b8d71260d4717c17a4e2",
+    "url": "/npuzzles/static/css/main.57d3f6e6.chunk.css"
+  },
+  {
+    "revision": "142b83b482febb70c637",
+    "url": "/npuzzles/static/js/2.20f73be0.chunk.js"
+  },
+  {
+    "revision": "b8d71260d4717c17a4e2",
+    "url": "/npuzzles/static/js/main.18db7647.chunk.js"
+  },
+  {
+    "revision": "be8bc6c1b65efd78e5ef",
+    "url": "/npuzzles/static/js/runtime-main.b26d06fc.js"
+  }
+]);
